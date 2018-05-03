f6a2b7288680c283e5666a8753a52ed02adf1495
muzhichkov_ra <chaika1201@gmail.com>
2018-05-03 09:17:23 +0400

    data from website

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,60 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButtonGetData, SIGNAL(clicked()), this, SLOT(buttonGetDataClicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::buttonGetDataClicked()
+{
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    connect(manager, SIGNAL(finished(QNetworkReply*)),
+            this, SLOT(replyFinished(QNetworkReply*)));
+
+    manager->get(QNetworkRequest(QUrl("https://www.betgamesafrica.co.za/ext/game/results/testpartner/2018-04-29/1/1")));
+}
+
+void MainWindow::buttonComputeClicked()
+{
+
+}
+
+void MainWindow::replyFinished(QNetworkReply *reply)
+{
+    QByteArray dataFromPage = reply->readAll();
+    QString stringFromPage(dataFromPage);
+//    qDebug() << dataFromPage.size();
+//    QString htmlString(dataFromPage);
+//    QString plainTextString = QTextDocumentFragment::fromHtml(htmlString).toPlainText();
+//    qDebug() << plainTextString;
+//    stringFromPage = stringFromPage.mid(stringFromPage.indexOf("Video"));
+//    QTextDocument *textDocumentFromPage = new QTextDocument(stringFromPage);
+
+//    QString plainTextString = QTextDocumentFragment::fromHtml(stringFromPage).toPlainText();
+//    plainTextString = plainTextString.mid(plainTextString.indexOf("Video"));
+//    //ui->textEdit->setPlainText(plainTextString);
+//    QStringList unparsedList = plainTextString.split("\n");
+//    QStringList parsedList;
+//    unparsedList.removeAt(0);
+//    unparsedList.removeAll(QString("Watch "));
+//    unparsedList.removeAll(QString("7 out of 42"));
+//    unparsedList.removeLast();
+//    int num = 1;
+//    foreach (QString s, unparsedList)
+//    {
+//        ui->textEditResult->append(num + " -> " + s);
+//        num++;
+//    }
+    reply->deleteLater();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QTextDocument>
+#include <QTextDocumentFragment>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +21,11 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+public slots:
+    void buttonGetDataClicked();
+    void buttonComputeClicked();
+    void replyFinished(QNetworkReply *reply);
 };
 
 #endif // MAINWINDOW_H
